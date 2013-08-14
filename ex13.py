f9b3abbf49004035263111a81212518a33f954e1
Jamie Fletcher <jamiemfletcher@gmail.com>
2013-08-14 13:30:42 +0100

    Added Exercises 2 - 16

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, dogs, cats, birds = argv
+
+print "The script is called:", script
+print "Your first variable is called:", dogs
+print "Your second variable is called:", cats
+print "Your third variable is called:", birds
---
+++ ex2.py
@@ -0,0 +1,9 @@
+# This is a comment
+# this is ignored by python
+
+print "I could have code like this." # and this is ignored
+
+# disable lines with a comment, like this
+# print "This won't run."
+
+print "This will run."
